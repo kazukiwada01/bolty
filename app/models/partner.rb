6d0f6f8b11e8025c936670ee70e1cbd31ebff474
kazukiwada01 <92724990+kazukiwada01@users.noreply.github.com>
2022-02-06 02:19:13 +0900

    バリデーションの設定

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -1,2 +1,15 @@
 class Partner < ApplicationRecord
+  validates :company, presence: true
+  validates :email, presence: true
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' } do
+    validates :first_name
+    validates :last_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters' } do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
+  validates :phone_number, presence: true
+  validates :phone_number, length: { minimum: 10, maximum: 11, message: 'is too short' }
+  validates :phone_number, numericality: { with: /\A[0-9]+\z/, message: 'is invalid. Input only numbers' }
 end
