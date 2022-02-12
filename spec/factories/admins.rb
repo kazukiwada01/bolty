4f007175b788857212250ff689a52c1b9d024ddb
kazukiwada01 <92724990+kazukiwada01@users.noreply.github.com>
2022-02-12 14:17:20 +0900

    モデルテストの実装

--- spec/factories/admins.rb
+++ spec/factories/admins.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :admin do
-    
+    company { 'bolty' }
+    email { Faker::Internet.free_email }
+    password { Faker::Internet.password(min_length: 6) }
+    last_name { '山田' }
+    first_name { '太郎' }
+    last_name_kana { 'ヤマダ' }
+    first_name_kana { 'タロウ' }
+    phone_number { '09012345678' }
   end
 end
