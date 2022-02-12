e8d88532266c9da2bf5b7baba3e85fe3d944c5fe
kazukiwada01 <92724990+kazukiwada01@users.noreply.github.com>
2022-02-12 19:56:03 +0900

    バリデーションの設定

--- app/models/gym.rb
+++ app/models/gym.rb
@@ -4,4 +4,15 @@ class Gym < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
+
+  with_options presence: true do
+    validates :image
+    validates :name
+  end
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  with_options presence: true do
+    validates :city_name
+    validates :street
+    validates :gym_url
+  end
 end
