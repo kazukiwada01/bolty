dd1f77ca14d9ae8888e2fc23e89bab3450433eb5
kazukiwada01 <92724990+kazukiwada01@users.noreply.github.com>
2022-02-23 23:23:43 +0900

    gradeモデルの作成

---
+++ app/models/grade.rb
@@ -0,0 +1,12 @@
+class Grade < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '---' },
+    { id: 2, name: '6級' },
+    { id: 3, name: '5級' },
+    { id: 4, name: '4級' },
+    { id: 5, name: '3級' },
+    { id: 6, name: '2級' },
+    { id: 7, name: '1級' },
+    { id: 8, name: '初段' }
+  ]
+end
