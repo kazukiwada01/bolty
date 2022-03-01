d000ed6650219c2a8a5beac35cff5923d74eaca4
kazukiwada01 <92724990+kazukiwada01@users.noreply.github.com>
2022-03-01 23:55:37 +0900

    モデルテストの実装

--- spec/factories/problems.rb
+++ spec/factories/problems.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :problem do
-    
+    name           { '1番' }
+    grade_id       { 2 }
+    holding        { 1 }
+    physical       { 1 }
+    move           { 1 }
+    positioning    { 1 }
+    footwork       { 1 }
+    coordination   { 1 }
+    admin
+    problem_list
   end
 end
