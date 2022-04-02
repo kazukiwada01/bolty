ac18768d5f9320162ec3724312e71285d651d63f
kazukiwada01 <92724990+kazukiwada01@users.noreply.github.com>
2022-04-02 18:52:01 +0900

    ジム削除機能の修正

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -1,7 +1,7 @@
 class Problem < ApplicationRecord
   belongs_to :admin
   belongs_to :problem_list
-  has_many :results
+  has_many :results, dependent: :destroy
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :grade
--- app/models/problem_list.rb
+++ app/models/problem_list.rb
@@ -1,7 +1,7 @@
 class ProblemList < ApplicationRecord
   belongs_to :admin
   belongs_to :gym
-  has_many :problems
+  has_many :problems, dependent: :destroy
 
   validates :name, presence: true
 end
